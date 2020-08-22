4970e90c07f46dccbd8a77a7ac1d2fe9eb424ffd
Roberto Hanus Morales <robertohanus@hotmail.com>
2020-08-21 22:07:52 -0400

    Avance

--- main.c
+++ main.c
@@ -2,30 +2,38 @@
 #include <stdio.h>
 // #include "LITTLE.C"
 
-void main()
+void FMSoundInit()
 {
-            int i=1;
-            
-            i=i<<4;
+    NR52_REG = 0x80;
+    NR51_REG = 0x11;
+    NR50_REG = 0x77;
+    NR10_REG = 0x00;
+}
 
-            printf("%x",i);
+void FMSound_on(int freq)
+{
+    NR11_REG = 0x10;
+    NR12_REG = 0xF0;
+    NR13_REG = freq & 0xFF;
+    NR14_REG = ((freq >> 8) & 0x07) | 0x80;
+}
 
-            NR52_REG = 0x80;
-            NR51_REG = 0x11;
-            NR50_REG = 0x77;
-            NR10_REG = 0x00;
-                    
+void FMSound_off()
+{
+    NR12_REG = 0x00;
+}
+
+void main()
+{                  
+    FMSoundInit();  
     while(1)
     {
         unsigned char joypad_state = joypad();
         if(joypad_state)
         {
-            NR11_REG = 0x10;
-            NR12_REG = 0xF0;
-            NR13_REG = 0x00;
-            NR14_REG = 0x87;
-            delay(500);
-            NR12_REG = 0x00;
+            FMSound_on(523);
+            delay(500);     
+            FMSound_off();      
         }
     }
 
